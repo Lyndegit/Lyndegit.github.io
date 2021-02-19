6a9b794d742963ebcdb3846363cba69dab73d7b9
Lyndegit <2061554200@qq.com>
2021-02-19 12:04:02 +0800

    Site updated: 2021-02-19 12:04:00

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
           var data = res.list[j].arr;
           var liTmpl = "";
           for (var i = 0, len = data.link.length; i < len; i++) {
-            var minSrc = 'https://gitee.com/mrxun_jn/photosStores/raw/master/min_photos/' + data.link[i];
-            var src = 'https://gitee.com/mrxun_jn/photosStores/raw/master/photos/' + data.link[i];
+            var minSrc = 'https://raw.githubusercontent.com/Lyndegit/photoware/master/mini_photos/' + data.link[i];
+            var src = 'https://raw.githubusercontent.com/Lyndegit/photoware/master/photos/' + data.link[i];
             var type = data.type[i];
             var target = src + (type === 'video' ? '.mp4' : '.jpg');
             src += '';
